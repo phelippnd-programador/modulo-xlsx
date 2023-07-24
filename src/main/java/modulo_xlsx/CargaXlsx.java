91b681840a782769c02233fb1b9bd9c242681610
Phelipp Nascimento <pdamasceno@pscs.com.br>
2023-07-24 20:12:35 -0300

    adicionando versão ao projeto

--- src/main/java/modulo_xlsx/CargaXlsx.java
+++ src/main/java/modulo_xlsx/CargaXlsx.java
@@ -6,25 +6,26 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class CargaXlsx {
-	public final String CAMINHO_WORKSHEET ="xl/worksheets/";
-	public String carga(String nome,String nomeSheet) throws IOException {
+	public final String CAMINHO_WORKSHEET = "xl/worksheets/";
+
+	public String carga(String nome, String nomeSheet) throws IOException {
 		StringBuilder sb = new StringBuilder();
-		FileInputStream fileInputStream = new FileInputStream(nome);
-		ZipInputStream zis = new ZipInputStream(fileInputStream);
-		ZipEntry entrada;
-		while ((entrada =  zis.getNextEntry()) != null) {
-			if(entrada.getName().equals(String.format("%s/%s.xml",CAMINHO_WORKSHEET, nomeSheet))) {
-				byte[] buffer = new byte[1024];
-				int byteRead;
-				while((byteRead = zis.read(buffer))!=-1) {
-					sb.append(new String(buffer,0,byteRead));
+		try (FileInputStream fileInputStream = new FileInputStream(nome)) {
+			try (ZipInputStream zis = new ZipInputStream(fileInputStream)) {
+
+				ZipEntry entrada;
+				while ((entrada = zis.getNextEntry()) != null) {
+					if (entrada.getName().equals(String.format("%s/%s.xml", CAMINHO_WORKSHEET, nomeSheet))) {
+						byte[] buffer = new byte[1024];
+						int byteRead;
+						while ((byteRead = zis.read(buffer)) != -1) {
+							sb.append(new String(buffer, 0, byteRead));
+						}
+					}
+
 				}
-				return sb.toString();
 			}
-			
 		}
-		zis.close();
-		fileInputStream.close();
 		return sb.toString();
 	}
 }
